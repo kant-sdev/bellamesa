0e5002ebf268872cb95558a3af87f0002c3fc1b2
kant-dev <kaua.cantanhede.santos@gmail.com>
2025-03-05 19:52:15 -0300

    atualização de Card

--- components/layout/Cards/CardProduto.tsx
+++ components/layout/Cards/CardProduto.tsx
@@ -5,6 +5,8 @@ type Props = {
 
 export default  function CardProduto({ category }: Props) {
 
+    console.log(category)
+
     return (
         <>
             <h2>Card dos produtos em breve...</h2>
